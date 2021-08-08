40cf0ab4491851acd3dcc393594d4eac7c28ff3c
Seweryn <seweryn87@gmail.com>
2021-08-08 11:40:23 +0200

    Initial commit

---
+++ src/main/java/pl/sda/springdodatkowe/SpringDodatkoweApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.springdodatkowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDodatkoweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDodatkoweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/springdodatkowe/SpringDodatkoweApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springdodatkowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDodatkoweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
